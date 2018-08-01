0cb63bf14986224a93a39c7f2bb5358ffd6e3f8b
Benjamin T. Vincent <drbenvincent@users.noreply.github.com>
2018-08-01 11:54:09 +0100

    fixed #24... was trying to plot true param values where there were none

--- bad/triplot.py
+++ bad/triplot.py
@@ -4,39 +4,42 @@ import logging
 
 def tri_plot(θ, filename, θ_true=None, priors=None):
 
-    keys = θ.keys()
+    logging.debug('Entered tri_plot.py')
     n_params = len(priors)
     
     fig, axes = plt.subplots(n_params, n_params, 
         figsize=(9, 9), tight_layout=True, sharex='col')
 
     # plot marginal histograms on the diagonal
-    for n, key in enumerate(keys):
+    for n, key in enumerate(θ.keys()):
         axes[n, n].hist(θ[key], 30, density=1, facecolor='green', alpha=0.75)
         axes[n, n].set_xlabel(key)
         if θ_true is not None:
             axes[n, n].axvline(x=θ_true[key][0], color='k', linestyle='-')
 
+    logging.debug('Plotted marginal histograms ok')
+
     # now plot bivariate scatter in the lower triangle
-    for row in range(n_params):
-        row_key = keys[row]
+    for row, row_key in enumerate(θ.keys()):
+        for col, col_key in enumerate(θ.keys()):
+            logging.debug(f'row/col = {row, col}')
 
-        for col in range(n_params):
-            col_key = keys[col]
-
+            # bivariate scatter plots in lower triangle
             if col < row:
                 axes[row, col].scatter(θ[col_key], θ[row_key], alpha=0.1)
-
-                axes[row, col].axvline(
-                    x=θ_true[col_key][0], color='k', linestyle='-')
-                axes[row, col].axhline(
-                    y=θ_true[row_key][0], color='k', linestyle='-')
-
+                if θ_true is not None:
+                    axes[row, col].axvline(
+                        x=θ_true[col_key], color='k', linestyle='-')
+                    axes[row, col].axhline(
+                        y=θ_true[row_key], color='k', linestyle='-')
                 axes[row, col].set_xlabel(col_key)
                 axes[row, col].set_ylabel(row_key)
             
+            # remove axes in the upper triangle
             if col > row:
                 axes[row, col].remove()
+
+    logging.debug('Plotted bivariate scatter plots ok')
 
     savename = filename + '_parameter_plot.pdf'
     plt.savefig(savename)
