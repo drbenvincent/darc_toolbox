faa0fa3072092ee307e8ba3221667af3fc68876e
Benjamin T. Vincent <drbenvincent@users.noreply.github.com>
2018-08-09 17:48:54 +0100

    #33 removing some code from /dev which is either obsolete or done with tests now

--- dev/debugging_optimisation.py
+++
@@ -1,9 +0,0 @@
-from darc.delayed import models
-from darc.designs import Kirby2009, Frye, DARCDesign
-
-
-design_thing = DARCDesign()
-model = models.Hyperbolic(n_particles=5_000)  # was 50_000
-design = design_thing.get_next_design(model)
-print(design)
-print('SUCESS 😀')
