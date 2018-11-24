887f2c7a083b453efe948d90bd315dea405465cd
Benjamin T. Vincent <drbenvincent@users.noreply.github.com>
2018-11-24 16:37:21 +0000

    basic darc_parameter_recovery now working #6

--- dev/darc_parameter_recovery.py
+++ dev/darc_parameter_recovery.py
@@ -8,83 +8,78 @@ from darc.delayed import models
 from darc.designs import Kirby2009, Frye, DARCDesign
 import pandas as pd
 import numpy as np
+from copy import copy
 
 
-def parameter_recovery(θ_true, design_method, model_type, n_particles=10_000, n_trials=20):
-
-    print('parameter recovery')
-    design_thing = design_method()
-    model = model_type(n_particles=n_particles)
-    # set true model parameters, as a dataframe
-    model.θ_true = θ_true
-
-    for _ in range(n_trials):
-        design = design_thing.get_next_design(model)
-        response = model.get_simulated_response( darc.single_design_tuple_to_df(design))
-        design_thing.enter_trial_design_and_response(design, response)
-        model.update_beliefs(design_thing.all_data)
-
-    return model.get_θ_point_estimate()
-
-
-def parameter_recovery_sweep(sweep_θ_true, model_type):
-
+def parameter_recovery_sweep(sweep_θ_true, model, design_thing):
     print('starting parameter recovery sweep')
-
+    
     # TODO: iterate over rows of pandas df
     rows, _ = sweep_θ_true.shape
 
+    θ_estimated_list = []
+    
     for row in range(rows):
-        θ_true = sweep_θ_true.loc[[row]]
+        
+        # for every true set of parameters, make a local copy of the model and design thing
+        local_model = copy(model)
+        local_design_thing = copy(design_thing)
 
-        # TODO: repeat the parameter recovery a number of times
-        # TODO: provide as a generic function to call for the given inputs
-        θ_estimated = parameter_recovery(θ_true, 
-                                         design_method,
-                                         model_type,
-                                         n_particles,
-                                         n_trials)
+        # set true parameter values
+        local_model.θ_true = sweep_θ_true.loc[[row]]
 
-        print(θ_true - θ_estimated)
+        # print(f'true parameters: {local_model.θ_true}')
+              
+        # TODO: append θ_estimated to a new row in a dataframe
+        θ_estimated = parameter_recovery(local_design_thing, local_model)
+        
+        # print(f'estimated parameters: {θ_estimated}')
+        
+        θ_estimated_list.append(θ_estimated)
+
+    return pd.concat(θ_estimated_list)
 
 
+def parameter_recovery(design_thing, model):
+    model = simulated_experiment_trial_loop(design_thing, model)
+    return model.get_θ_point_estimate()
 
-# Run a single parameter recovery
-θ_true = pd.DataFrame.from_dict({'logk': [-7], 'α': [3]})
 
-θ_estimated = parameter_recovery(θ_true,
-                                 model_type=models.Hyperbolic,
-                                 n_trials=10)
+def simulated_experiment_trial_loop(design_thing, model):
+    '''run a simulated experiment trial loop'''
+    for trial in range(666):
+        design = design_thing.get_next_design(model)
+
+        if design is None:
+            break
+        
+        design_df = darc.single_design_tuple_to_df(design)
+        response = model.get_simulated_response(design_df)
+        design_thing.enter_trial_design_and_response(design, response)
+
+        model.update_beliefs(design_thing.all_data)
+    
+    return model
+
+
+# DOWN TO BUSINESS =======================================================
+# do param recovery for hyperbolic time discounting
+
+max_trials = 20
+
+# create a design thing
+design_thing = DARCDesign(max_trials=max_trials,
+                          RA=list(100*np.linspace(0.05, 0.95, 19)),
+                          random_choice_dimension='DB')
+
+# create a model
+model = models.Hyperbolic(n_particles=5000) 
+
+# set up a dataframe of true parameter combinations to run through
+θsweep = pd.DataFrame.from_dict({'logk': [-2, -3, -4], 
+                                 'α': [3, 3, 3]})
+                                
+# do the parameter sweep
+θ_estimated = parameter_recovery_sweep(θsweep, model, design_thing)
 
 print(θ_estimated)
-print(θ_true - θ_estimated)
-
-# TODO: define sweep as a list of dicts so we can do list comprehensions
-
-# Run a parameter sweep
-# First we create a pandas table, each row is one set of true params for the sweep
-sweep_θ_true = pd.DataFrame.from_dict(
-    {'logk': np.linspace(-7, -2, 5), 'α': [3, 3, 3, 3, 3]})
-
-parameter_recovery_sweep(sweep_θ_true)
-
-
-
-
-
-
-# NOTE: So this is ok, but it requires knowledge of the parameters and their order 
-# within the parameter sweep.
-# It might be best to set this up ONCE as a dataframe then just process it.
-
-# define params I want
-logk_list = np.linspace(-7, -2, 5)
-alpha_list = [3, 3, 3, 3, 3]
-
-# do a parameter sweep
-for logk, alpha in zip(logk_list, alpha_list):
-    param_dict = {'logk': [logk], 'α': [alpha]}
-    print(param_dict)
-    
-    param_df = pd.DataFrame.from_dict(param_dict)
-    print(param_df)
