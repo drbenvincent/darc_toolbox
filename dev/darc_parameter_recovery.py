2b8cf2f234247bc88e96e89ebbdb8afcea628bc5
Benjamin T. Vincent <drbenvincent@users.noreply.github.com>
2018-11-24 18:39:35 +0000

    #6 added notebook to produce first parameter sweep plot

--- dev/darc_parameter_recovery.py
+++ dev/darc_parameter_recovery.py
@@ -13,28 +13,15 @@ from copy import copy
 
 def parameter_recovery_sweep(sweep_θ_true, model, design_thing):
     print('starting parameter recovery sweep')
-    
-    # TODO: iterate over rows of pandas df
     rows, _ = sweep_θ_true.shape
-
     θ_estimated_list = []
-    
     for row in range(rows):
-        
-        # for every true set of parameters, make a local copy of the model and design thing
+        # make local copies
         local_model = copy(model)
         local_design_thing = copy(design_thing)
-
         # set true parameter values
-        local_model.θ_true = sweep_θ_true.loc[[row]]
-
-        # print(f'true parameters: {local_model.θ_true}')
-              
-        # TODO: append θ_estimated to a new row in a dataframe
-        θ_estimated = parameter_recovery(local_design_thing, local_model)
-        
-        # print(f'estimated parameters: {θ_estimated}')
-        
+        local_model.θ_true = sweep_θ_true.loc[[row]]              
+        θ_estimated = parameter_recovery(local_design_thing, local_model)        
         θ_estimated_list.append(θ_estimated)
 
     return pd.concat(θ_estimated_list)
@@ -60,26 +47,3 @@ def simulated_experiment_trial_loop(design_thing, model):
         model.update_beliefs(design_thing.all_data)
     
     return model
-
-
-# DOWN TO BUSINESS =======================================================
-# do param recovery for hyperbolic time discounting
-
-max_trials = 20
-
-# create a design thing
-design_thing = DARCDesign(max_trials=max_trials,
-                          RA=list(100*np.linspace(0.05, 0.95, 19)),
-                          random_choice_dimension='DB')
-
-# create a model
-model = models.Hyperbolic(n_particles=5000) 
-
-# set up a dataframe of true parameter combinations to run through
-θsweep = pd.DataFrame.from_dict({'logk': [-2, -3, -4], 
-                                 'α': [3, 3, 3]})
-                                
-# do the parameter sweep
-θ_estimated = parameter_recovery_sweep(θsweep, model, design_thing)
-
-print(θ_estimated)
