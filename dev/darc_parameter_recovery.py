98e1c73e06f050913f524741e9b72b3d86a5ef6a
Benjamin T. Vincent <drbenvincent@users.noreply.github.com>
2018-11-26 11:58:36 +0000

    #6 minor changes to darc_parameter_recovery.py

--- dev/darc_parameter_recovery.py
+++ dev/darc_parameter_recovery.py
@@ -1,8 +1,3 @@
-# Point Python to the path where we have installed the bad and darc packages
-import sys
-sys.path.insert(0, '/Users/btvincent/git-local/darc-experiments-python')
-
-
 import darc
 from darc.delayed import models
 from darc.designs import Kirby2009, Frye, DARCDesign
@@ -20,20 +15,22 @@ def parameter_recovery_sweep(sweep_θ_true, model, design_thing):
         local_model = copy(model)
         local_design_thing = copy(design_thing)
         # set true parameter values
-        local_model.θ_true = sweep_θ_true.loc[[row]]              
-        θ_estimated = parameter_recovery(local_design_thing, local_model)        
+        local_model.θ_true = sweep_θ_true.loc[[row]]  
+
+        fitted_model = simulated_experiment_trial_loop(local_design_thing, local_model)
+        θ_estimated = fitted_model.get_θ_point_estimate()
+
         θ_estimated_list.append(θ_estimated)
 
     return pd.concat(θ_estimated_list)
 
 
-def parameter_recovery(design_thing, model):
-    model = simulated_experiment_trial_loop(design_thing, model)
-    return model.get_θ_point_estimate()
-
 
 def simulated_experiment_trial_loop(design_thing, model):
     '''run a simulated experiment trial loop'''
+
+    # TODO: Add a check here to confirm that the model has some θ_true
+
     for trial in range(666):
         design = design_thing.get_next_design(model)
 
@@ -42,6 +39,9 @@ def simulated_experiment_trial_loop(design_thing, model):
         
         design_df = darc.single_design_tuple_to_df(design)
         response = model.get_simulated_response(design_df)
+
+        # TODO: DOUBLE CHECK THIS IS ACTUALLY UPDATING STUFF!! OR DO WE NEED TO 
+        # SPECIFICALLY UPATE THE design_thing AND model ????
         design_thing.enter_trial_design_and_response(design, response)
 
         model.update_beliefs(design_thing.all_data)
