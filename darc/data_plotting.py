0b5f7a79c32244f1a42535532995d77d3333b58f
Benjamin T. Vincent <drbenvincent@users.noreply.github.com>
2018-11-27 13:46:31 +0000

    #52 improved plotting. design_type_testing.ipynb

--- darc/data_plotting.py
+++ darc/data_plotting.py
@@ -36,24 +36,24 @@ def all_data_plotter(all_data):
         # delay based plot only
         f, ax = plt.subplots(1, 1, figsize=(9, 4))
         if front_end_delays:
-            plot_delay_with_front_end_delays(ax, RA, DA, RB, DB, R)
+            plot_delay_with_front_end_delays(ax, all_data)
         else:
-            plot_delay_without_front_end_delays(ax, RA, DA, RB, DB, R)
+            plot_delay_without_front_end_delays(ax, all_data)
 
     elif risky_choices and not delayed_choices:
         # risky based plot only
         f, ax = plt.subplots(1, 1, figsize=(9, 4))
-        plot_probability_data(ax, RA, PA, RB, PB, R)
+        plot_probability_data(ax, all_data)
 
     elif delayed_choices and risky_choices:
         # both risky and delayed plots
         f, axes = plt.subplots(1, 2, figsize=(9, 4), tight_layout=True)
         if front_end_delays:
-            plot_delay_with_front_end_delays(axes[0], RA, DA, RB, DB, R)
+            plot_delay_with_front_end_delays(axes[0], all_data)
         else:
-            plot_delay_without_front_end_delays(axes[0], RA, DA, RB, DB, R)
+            plot_delay_without_front_end_delays(axes[0], all_data)
 
-        plot_probability_data(axes[1], RA, PA, RB, PB, R)
+        plot_probability_data(axes[1], all_data)
 
     # if filename is not None:
     #     savename = filename + '_data_plot.pdf'
@@ -63,7 +63,16 @@ def all_data_plotter(all_data):
 
 # DELAY PLOTS =========================================================
 
-def plot_delay_with_front_end_delays(ax, RA, DA, RB, DB, R):
+def plot_delay_with_front_end_delays(ax, alldata):
+
+    RA = all_data.RA.values
+    DA = all_data.DA.values
+    PA = all_data.PA.values
+    RB = all_data.RB.values
+    DB = all_data.DB.values
+    PB = all_data.PB.values
+    R = all_data.R.values
+
     # plot lines between pairs of prospects
     n_points = len(R)
     for t in range(n_points):
@@ -82,25 +91,43 @@ def plot_delay_with_front_end_delays(ax, RA, DA, RB, DB, R):
     ax.set_xlabel('delay (days)')
     ax.set_ylabel('reward')
     ax.set_xscale('log')
-    ax.set_title('Plot for data with front-end delays')
+    #ax.set_title('Plot for data with front-end delays')
 
 
-def plot_delay_without_front_end_delays(ax, RA, DA, RB, DB, R):
+def plot_delay_without_front_end_delays(ax, all_data):
+
+    RA = all_data.RA.values
+    DA = all_data.DA.values
+    PA = all_data.PA.values
+    RB = all_data.RB.values
+    DB = all_data.DB.values
+    PB = all_data.PB.values
+    R = all_data.R.values
+
     ax.scatter(x=DB[R == 0], y=RA[R == 0] / RB[R == 0], 
                c='b', alpha=0.5, label='chose A')
     ax.scatter(x=DB[R == 1], y=RA[R == 1] / RB[R == 1],
                c='r', alpha=0.5, label='chose B')
     ax.legend()
     ax.set_xlabel('delay (days)')
-    ax.set_ylabel('RA/RB')
+    ax.set_ylabel('$R^A/R^B$')
     ax.set_xscale('linear')
-    ax.set_title('Plot for data without front-end delays')
+    #ax.set_title('Plot for data without front-end delays')
 
 
 # RISKY PLOTS =========================================================
 
 
-def plot_probability_data(ax, RA, PA, RB, PB, R):
+def plot_probability_data(ax, alldata):
+
+    RA = all_data.RA.values
+    DA = all_data.DA.values
+    PA = all_data.PA.values
+    RB = all_data.RB.values
+    DB = all_data.DB.values
+    PB = all_data.PB.values
+    R = all_data.R.values
+
     ax.scatter(x=PB[R == 0], y=RA[R == 0] / RB[R == 0],
                c='b', alpha=0.5, label='chose A')
     ax.scatter(x=PB[R == 1], y=RA[R == 1] / RB[R == 1],
