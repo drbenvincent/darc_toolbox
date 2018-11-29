982f090f62824bf155e693615122b5482c0250cf
Benjamin T. Vincent <drbenvincent@users.noreply.github.com>
2018-11-29 21:27:30 +0000

    #57 can now do multiple model fitting :thumbsup:

--- dev/darc_parameter_recovery.py
+++ dev/darc_parameter_recovery.py
@@ -6,6 +6,8 @@ import pandas as pd
 import numpy as np
 from copy import copy
 from scipy.stats import norm
+import random
+
 
 def parameter_recovery_sweep(sweep_θ_true, model, design_thing, target_param_name):
     print('starting parameter recovery sweep')
@@ -74,3 +76,34 @@ def simulated_experiment_trial_loop(design_thing, fitted_model, response_model=N
         summary_stats = None
     
     return fitted_model, summary_stats
+
+
+def simulated_multi_experiment(design_thing, models_to_fit, response_model):
+    '''Simulate an experiment where we have one response model, but have mulitple models
+    which we do parameter estimation with and get designs from.'''
+
+    if response_model.θ_true is None:
+        raise ValueError('response_model must have θ_true values set')
+
+    n_models = len(models_to_fit)
+
+    for trial in range(666):
+
+        
+        # get the design from a random model
+        m = random.randint(0,n_models-1)
+        design = design_thing.get_next_design(models_to_fit[m])
+        if design is None:
+            break
+        
+        print(f'trial {trial}, design from model: {m}')
+
+        # get response from response model
+        design_df = darc.single_design_tuple_to_df(design)
+        response = response_model.get_simulated_response(design_df)
+        design_thing.enter_trial_design_and_response(design, response)
+
+        # update beliefs of all models
+        [model.update_beliefs(design_thing.all_data) for model in models_to_fit]
+    
+    return models_to_fit, design_thing
