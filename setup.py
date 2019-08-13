1a4fc38a28b3dfb9655ca0aa8d63948ac6a4395d
Benjamin T. Vincent <drbenvincent@users.noreply.github.com>
2019-08-13 14:05:50 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="darc_toolbox",
+    version="0.0.1",
+    author="Benjamin T. Vincent",
+    author_email="b.t.vincent@dundee.ac.uk",
+    description="Delayed And Risky Choice Toolbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drbenvincent/darc_toolbox",
+    keywords = ["delay discounting", "risky choice", "psychological experiments". "bayesian", "adaptive design", "inference"],
+    packages=['darc_toolbox'],
+    install_requires=['badapted>=0.0.2'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
